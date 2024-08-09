9715b0bff582b5216a69960694b387ece02d37b3
tazrilparveezali <tazrilparvez96@gmail.com>
2024-08-10 00:30:04 +0530

    modules and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
-const tailwindcss = require('tailwindcss');
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
     plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+        tailwindcss,
+        autoprefixer
     ]
 };
