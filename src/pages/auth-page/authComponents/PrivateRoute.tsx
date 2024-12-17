c370f1c7ac01c110da56fd6b49847d1dec4d9e1e
Tazril Ali <tazril.ali@goripples.com>
2024-12-17 18:49:00 +0530

    private route

--- src/pages/auth-page/authComponents/PrivateRoute.tsx
+++ src/pages/auth-page/authComponents/PrivateRoute.tsx
@@ -4,21 +4,24 @@ import { useRecoilState } from "recoil";
 import { siteConfig } from "@/config/site";
 import { authTokenState } from "@/recoil/authTokenState";
 import { userIdState } from "@/recoil/userIdState";
+import { useEffect } from "react";
 
 const PrivateRoute = () => {
   const [authToken, setAuthToken] = useRecoilState(authTokenState);
   const [userId, setUserId] = useRecoilState(userIdState);
 
-  const storedToken = localStorage.getItem("authToken");
-  const storedUserId = localStorage.getItem("userId");
+  useEffect(() => {
+    const storedToken = localStorage.getItem("authToken");
+    const storedUserId = localStorage.getItem("userId");
 
-  // Update Recoil state only if the stored values are different
-  if (authToken !== storedToken) {
-    setAuthToken(storedToken || "");
-  }
-  if (userId !== storedUserId) {
-    setUserId(storedUserId || "");
-  }
+    // Update Recoil state only if the stored values are different
+    if (authToken !== storedToken) {
+      setAuthToken(storedToken || "");
+    }
+    if (userId !== storedUserId) {
+      setUserId(storedUserId || "");
+    }
+  }, []);
 
   return authToken ? <Outlet /> : <Navigate to={siteConfig.path.signIn} />;
 };
