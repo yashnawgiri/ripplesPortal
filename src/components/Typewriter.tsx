86729af92b190dec347366e5abcdadef2275b1f3
= <=>
2025-01-15 18:25:53 +0530

    add typewriter component

---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+type Props = {
+    words : string[],
+    typingSpeed? : number,
+    deletingSpeed? : number,
+    pauseTime? : number
+}
+
+const Typewriter = ({ words, typingSpeed = 50, deletingSpeed = 10, pauseTime = 1000 }:Props) => {
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [displayedText, setDisplayedText] = useState('');
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    let typingTimeout;
+
+    const currentWord = words[currentWordIndex];
+
+    if (isDeleting) {
+      // Remove a character
+      typingTimeout = setTimeout(() => {
+        setDisplayedText(currentWord.slice(0, displayedText.length - 1));
+      }, deletingSpeed);
+    } else {
+      // Add a character
+      typingTimeout = setTimeout(() => {
+        setDisplayedText(currentWord.slice(0, displayedText.length + 1));
+      }, typingSpeed);
+    }
+
+    // Handle when typing or deleting finishes
+    if (!isDeleting && displayedText === currentWord) {
+      typingTimeout = setTimeout(() => setIsDeleting(true), pauseTime);
+    } else if (isDeleting && displayedText === '') {
+      setIsDeleting(false);
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }
+
+    return () => clearTimeout(typingTimeout);
+  }, [displayedText, isDeleting, currentWordIndex, words, typingSpeed, deletingSpeed, pauseTime]);
+
+  return <div className="typewriter bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent">{displayedText}<span className="cursor bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent animate-blink">|</span></div>;
+};
+
+export default Typewriter;
