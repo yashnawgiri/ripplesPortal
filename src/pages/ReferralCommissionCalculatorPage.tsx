888ed2a379334f56e8521d6d1b5c00c1df8d0032
= <=>
2025-03-15 20:33:50 +0530

    Add Referral Commission Calculator page component with default layout

---
+++ src/pages/ReferralCommissionCalculatorPage.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/layouts/default";
+import ReferralCommissionCalculator from "@/components/ReferralCommissionCalculator";
+
+export default function ReferralCommissionCalculatorPage() {
+  return (
+    <DefaultLayout>
+      <section className="main-section shadow-none">
+       <ReferralCommissionCalculator/>
+      </section>
+    </DefaultLayout>
+  );
+}
