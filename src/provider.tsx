107e945d5931769a81eb9e87e388f33d9a95ab93
= <=>
2025-05-30 15:26:30 +0530

    Refactor Provider component: remove unused navigate prop from NextUIProvider and simplify the code structure. Add Newsletter component to HomePage for enhanced user engagement.

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,13 +1,10 @@
 import { NextUIProvider } from "@nextui-org/system";
-import { useNavigate } from "react-router-dom";
 import { DirectionProvider } from "@radix-ui/react-direction";
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  const navigate = useNavigate();
-
   return (
     <DirectionProvider dir="ltr">
-      <NextUIProvider navigate={navigate}>{children}</NextUIProvider>
+      <NextUIProvider>{children}</NextUIProvider>
     </DirectionProvider>
   );
 }
