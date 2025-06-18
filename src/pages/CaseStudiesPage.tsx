60e48580722ac101e78ce4b361fb2e23eaa03af7
= <=>
2025-06-18 18:27:40 +0530

    Add CaseStudy component and CaseStudiesPage to enhance case study presentation and navigation

---
+++ src/pages/CaseStudiesPage.tsx
@@ -0,0 +1,11 @@
+import CaseStudy from "@/components/caseStudy/CaseStudy";
+import DefaultLayout from "@/layouts/default";
+
+export default function CaseStudiesPage() {
+ 
+  return (
+    <DefaultLayout>
+      <CaseStudy />
+    </DefaultLayout>
+  );
+}
