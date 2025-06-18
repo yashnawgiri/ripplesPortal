e40010161edb0e13b1f526cec36ed5fef11697c6
= <=>
2025-06-18 18:35:00 +0530

    Remove unnecessary whitespace in CaseStudiesPage component for cleaner code.

--- src/pages/CaseStudiesPage.tsx
+++ src/pages/CaseStudiesPage.tsx
@@ -2,7 +2,6 @@ import CaseStudy from "@/components/caseStudy/CaseStudy";
 import DefaultLayout from "@/layouts/default";
 
 export default function CaseStudiesPage() {
- 
   return (
     <DefaultLayout>
       <CaseStudy />
