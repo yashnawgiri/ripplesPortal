87ac09d3c9fa2ad1cac1bbf3be2d4969bfe39879
yaxhxv <yashnawgiri45.com>
2025-07-16 16:05:22 +0530

    Remove unused local influencer images and update to use cloud-based images

--- src/assets/influencerImages/index.ts
+++ src/assets/influencerImages/index.ts
@@ -1,52 +1,36 @@
 /* eslint-disable max-len */
 
-// Local images from the influencerImages folder
-export const LOCAL_INFLUENCER_IMAGES = [
-  "/src/assets/influencerImages/User default.png",
-  "/src/assets/influencerImages/Sponsored default.png",
-  "/src/assets/influencerImages/Feed Post - 1_1 photo (1).png",
-  "/src/assets/influencerImages/Feed Post - 1_1 photo.png",
-  "/src/assets/influencerImages/Feed Post - Sponsored.png",
-  "/src/assets/influencerImages/Mockup (1).png",
-  "/src/assets/influencerImages/Mockup.png",
-  "/src/assets/influencerImages/Pull to refresh.png",
-];
-
 // Keep the original array for future use
+// export const LOCAL_CREATOR_IMAGES = [
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator1.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator2.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator3.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator4.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator5.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator6.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator7.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator8.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator9.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator10.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator13.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator14.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator15.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator16.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator17.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator18.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator19.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator20.jpg",
+//   "https://ripples1static.blob.core.windows.net/images/Influencer/creator21.jpg",
+// ];
 export const LOCAL_CREATOR_IMAGES = [
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator1.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator2.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator3.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator4.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator5.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator6.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator7.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator8.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator9.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator10.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator13.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator14.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator15.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator16.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator17.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator18.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator19.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator20.jpg",
-  "https://ripples1static.blob.core.windows.net/images/Influencer/creator21.jpg",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660985/User_default_l8raxp.png",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660984/Sponsored_default_k5xnda.png",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660983/Feed_Post_-_1_1_photo_1_u6y6wx.png",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660983/Pull_to_refresh_hbcpje.png",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660983/Mockup_bnlkgr.png",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660983/Mockup_1_fqoeka.png",
+  "https://res.cloudinary.com/dcgrzxf9v/image/upload/v1752660982/Feed_Post_-_Sponsored_wlhjvw.png",
 ];
-
-// Function to get a random local image
-export function getRandomLocalCreatorImage(): string {
-  return LOCAL_INFLUENCER_IMAGES[
-    Math.floor(Math.random() * LOCAL_INFLUENCER_IMAGES.length)
-  ];
-}
-
-// Function to get multiple random local images
-export function getRandomLocalCreatorImages(count: number): string[] {
-  const shuffled = [...LOCAL_INFLUENCER_IMAGES].sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, count);
-}
 
 // Function to get a random remote image (keeping for future use)
 export function getRandomRemoteCreatorImage(): string {
